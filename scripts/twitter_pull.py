ea60efc0aea8b60681f4a54fdcb2f1d890ae2868
Joe Gray <jgray@advancedpersistentsecurity.net>
2018-03-02 23:40:08 -0500

    Update twitter_pull.py

--- scripts/twitter_pull.py
+++ scripts/twitter_pull.py
@@ -1,48 +1,41 @@
-# This script is heavily based upon Justin Seitz's scripts in his Learning OSINT course on automatingosint.com
-from twitter_keys import *
- 
+# This script is heavily based upon Justin Seitz's scripts in his
+# Learning OSINT course on automatingosint.com
+
 import json
-import requests
-import time
 import sys
+import requests
+from twitter_keys import oauth, url
+
 # sys.setdefaultencoding() does not exist, here!
 reload(sys)  # Reload does the trick!
 sys.setdefaultencoding('UTF8')
 #
 # Download Tweets from a user profile
 #
-def download_tweets(screen_name, number_of_tweets, exclude_replies):
- 
-    api_url  = '%s/statuses/user_timeline.json' % base_twitter_url
- 
-    params  = {
+def download_tweets(screen_name, count, exclude_replies):
+    api_url = '%s/statuses/user_timeline.json' % url
+    params = {
         'screen_name':  screen_name,
-        'count':        number_of_tweets,
-	'replies':	exclude_replies,
+        'count':        count,
+        'replies':  exclude_replies,
     }
- 
     # send request to Twitter
     response = requests.get(api_url, params=params, auth=oauth)
- 
     if response.status_code == 200:
- 
         tweets = json.loads(response.content)
- 
         return tweets
- 
     return None
- 
- 
 # get a list of Tweets
-tweet_list = download_tweets('eccouncil', 150, 'true')
- 
+tweet_list = download_tweets('@C_3PJoe', 150, 'true')
+
 if tweet_list is not None:
- 
+
     # loop over each Tweet and print the date and text
     for tweet in tweet_list:
- 
+
         print ('%s : %s' % (tweet['created_at'], tweet['text']))
- 
+
 else:
- 
-    print '[*] No Tweets retrieved.'
+
+    print ('[*] No Tweets retrieved.')
+    
