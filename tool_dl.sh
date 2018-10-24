a9dbf7f1842950f84fcf4798251fdb97de9db31f
Joe Gray <jgray@advancedpersistentsecurity.net>
2018-10-24 03:39:29 -0400

    Create tool_dl.sh

---
+++ tool_dl.sh
@@ -0,0 +1,14 @@
+git clone https://github.com/PowerShell/PowerShell.git;
+git clone https://github.com/beefproject/beef.git;
+git clone https://github.com/rapid7/metasploit-framework.git;
+git clone https://github.com/EmpireProject/Empire.git;
+git clone https://github.com/Veil-Framework/Veil.git;
+git clone https://github.com/trustedsec/social-engineer-toolkit.git;
+git clone https://github.com/nullsecuritynet/tools.git;
+git clone https://github.com/savio-code/ghost-phisher.git;
+git clone https://github.com/backdoorfactory/backdoor-factory.git;
+git clone https://github.com/securestate/king-phisher.git;
+git clone https://github.com/SimplySecurity/SimplyTemplate.git;
+wget https://github.com/gophish/gophish/releases/download/0.7.1/gophish-v0.7.1-linux-64bit.zip;
+unzip gophish-v0.7.1-linux-64bit.zip;
+git clone https://github.com/ConsciousHacker/GreatSCT;
